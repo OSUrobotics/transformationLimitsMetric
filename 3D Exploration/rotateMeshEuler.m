d6aeb42825510fa1f6fd9af657ed26db1b0ccefc
Matthew Sundberg <matthberg@gmail.com>
2016-06-24 15:57:32 -0700

    fixed the rotateMeshEuler function

--- 3D Exploration/rotateMeshEuler.m
+++ 3D Exploration/rotateMeshEuler.m
@@ -1,7 +1,6 @@
 function [ verticesOut ] = rotateMeshEuler( verticesIn, alpha, beta, gamma )
-%ROTATEMESHEULER Summary of this function goes here
-%   Detailed explanation goes here
+%ROTATEMESHEULER using xyz euler rotational vectors, rotates the given set of points by that much
+%   Uses Eul2rotm to generate a rotation matrix and applies the rotation matrix to the points
 verticesOut = eul2rotm([alpha,beta,gamma])*verticesIn;
-
 end
 
