2d83c24bc2e9cb91bfc68c873944075489142441
RJSherman <rjsherman8888@gmail.com>
2016-06-24 14:13:52 -0700

    Converted the voxel test into a useable and commented function.  Yay!

---
+++ 3D Exploration/getVoxelisedVerts.m
@@ -0,0 +1,25 @@
+function [ output ] = getVoxelisedVerts( v,f )
+%Takes vertices and faces of a mesh and converts it to a list of voxel
+%points
+
+%Constants
+RESOLUTION = 0.5;
+
+%Add v and f to a struct
+fv.vertices = v;
+fv.faces = f;
+%Calculate the dimensions of the object
+xRange = range(fv.vertices(:,1));
+yRange = range(fv.vertices(:,2));
+zRange = range(fv.vertices(:,3));
+%Scale based on resolution
+xRes = ceil(xRange * RESOLUTION);
+yRes = ceil(yRange * RESOLUTION);
+zRes = ceil(zRange * RESOLUTION);
+%VOXELISE into a logical array
+OUT = VOXELISE(xRes,yRes,zRes,fv,'xyz');
+%Set x y and z index values
+[xIndeces,yIndeces,zIndeces] = ind2sub(size(OUT),find(OUT));
+output = [xIndeces yIndeces zIndeces];
+end
+
