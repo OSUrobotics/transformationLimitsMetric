8730b010b85eb9e3ea0120e3607d4e39fce38aa6
RJSherman <rjsherman8888@gmail.com>
2016-06-24 13:59:02 -0700

    Updated voxeliser test to be much faster

--- 3D Exploration/Mesh_voxelisation/Mesh_voxelisation/voxeliseTest.m
+++ 3D Exploration/Mesh_voxelisation/Mesh_voxelisation/voxeliseTest.m
@@ -1,16 +1,22 @@
-[v,f,no,na] = stlRead('PitcherAssm.stl')
-fv.vertices = v
-fv.faces = f
-OUT = VOXELISE(25,50,25,fv,'xyz')
+[v,f,no,na] = stlRead('PitcherAssm.stl');
+fv.vertices = v;
+fv.faces = f;
+
+xRange = range(fv.vertices(:,1));
+yRange = range(fv.vertices(:,2));
+zRange = range(fv.vertices(:,3));
+
+res = 0.25;
+xRes = ceil(xRange * res);
+yRes = ceil(yRange * res);
+zRes = ceil(zRange * res);
+
+OUT = VOXELISE(xRes,yRes,zRes,fv,'xyz');
 clf; 
 hold on;
-for i = 1:25
-    for j = 1:50
-        for k = 1:25
-            if OUT(i,j,k) == 1
-                scatter3(i,j,k,'.r');
-            end
-        end
-    end
-end
+[ryan1,ryan2,ryan3] = ind2sub(size(OUT),find(OUT));
+scatter3(ryan1,ryan2,ryan3,'k.');
+
 axis image
+grid on
+grid minor
