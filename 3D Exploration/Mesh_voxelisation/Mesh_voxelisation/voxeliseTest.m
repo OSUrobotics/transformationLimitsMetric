69156d2c8b35e88a129d6b416ddcedbc03d65163
RJSherman <rjsherman8888@gmail.com>
2016-06-24 14:07:29 -0700

    Added a new function that checks if points are inside a mesh

--- 3D Exploration/Mesh_voxelisation/Mesh_voxelisation/voxeliseTest.m
+++ 3D Exploration/Mesh_voxelisation/Mesh_voxelisation/voxeliseTest.m
@@ -6,7 +6,7 @@ xRange = range(fv.vertices(:,1));
 yRange = range(fv.vertices(:,2));
 zRange = range(fv.vertices(:,3));
 
-res = 0.25;
+res = 0.5;
 xRes = ceil(xRange * res);
 yRes = ceil(yRange * res);
 zRes = ceil(zRange * res);
@@ -14,8 +14,9 @@ zRes = ceil(zRange * res);
 OUT = VOXELISE(xRes,yRes,zRes,fv,'xyz');
 clf; 
 hold on;
-[ryan1,ryan2,ryan3] = ind2sub(size(OUT),find(OUT));
-scatter3(ryan1,ryan2,ryan3,'k.');
+
+[xIndeces,yIndeces,zIndeces] = ind2sub(size(OUT),find(OUT));
+scatter3(xIndeces,yIndeces,zIndeces,'k.');
 
 axis image
 grid on
