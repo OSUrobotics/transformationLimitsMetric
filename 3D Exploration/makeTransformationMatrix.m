22723cf4dd6e62c8f36867fa8d758e770ae4f881
Matthew Sundberg <matthberg@gmail.com>
2016-06-27 13:46:00 -0700

    added matrix creation script, actially works

--- 3D Exploration/makeTransformationMatrix.m
+++ 3D Exploration/makeTransformationMatrix.m
@@ -1,21 +1,9 @@
 function [ transformationMatrices ] = makeTransformationMatrix( transformationValues )
-%MAKETRANSFORMATIONMATRIX Generates translation matrices from a set of points to transform from the origin to 
-%   Inputs:
-% xyzSphereComplexity & angleSphereComplexity: values defining the number of divisions applied to the Icosahedron to generate the points in a sphere for either the normal vectors for rotation or the directional vectors for translation
-% angleDistribution: number of subdivisions of pi used in rotating the object on the plane defiend by the normal vector generated from the angle sphere, controled in complexity by the angleSphereComplexity
-%% Make the translation vectors using a uniform sphere
-moveSphere = IcosahedronMesh;
-translateSphere = SubdivideSphericalMesh(moveSphere,xyzSphereComplexity);
-translateVectors = translateSphere.vertices';
-%% Make a set of normal vectors using a sphere
-normalsSphere = SubdivideSphericalMesh(moveSphere,angleSphereComplexity);
-normalVectors = normalsSphere.vertices';
-%% Make the angle values for the angle rotation
-angleRotations = -pi:(2*pi)/angleDistribution:pi;
-%% Combine them into the possible combinations
-outputMatrix = combvec(translateVectors,normalVectors,angleRotations)';
-%% Convert those to transformation matrices
-transformationMatrices = zeros(4,4,size(outputMatrix,1));
-for index = 
+%MAKETRANSFORMATIONMATRIX Generates translation matrices from a set of points to transform from the origin
+%% Initialize container and loop through input values
+transformationMatrices = zeros(4,4,size(transformationValues,1));
+for index = 1:size(transformationValues,1)
+    transformationMatrices(:,:,index) = makehgtform('translate',transformationValues(index,1:3),'axisrotate',transformationValues(index,4:6),transformationValues(index,7));
+end
 end
 
