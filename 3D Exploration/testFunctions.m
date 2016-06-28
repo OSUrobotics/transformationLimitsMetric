595852e6a6db0be11f7ef674d26a2a5d32d4fd8a
Matthew Sundberg <matthberg@gmail.com>
2016-06-28 12:28:18 -0700

    updated sampling in test render

--- 3D Exploration/testFunctions.m
+++ 3D Exploration/testFunctions.m
@@ -1,5 +1,6 @@
 %% Load the files and prepare for graphing
 stlPath = 'stlTools/femur_binary.stl'; % sample object
+load('../transformationValues.mat');
 figure
 [vertices,faces,~,~] = stlRead(stlPath); % read stl in as faces and vertex
 %% General interesting information about file
@@ -8,15 +9,15 @@ fprintf('Centroid of Mesh: (%0.5f, %0.5f, %0.5f)\n',getCentroidMesh(vertices));
 
 %% Make transformation values
 % transformationValues = makeTransformationValues(2,2,5);
+ transformationValues = valuesIn;
+transformationValues = transformationValues(1:501:end,:);
 %% Make example object
 [x,y,z]=sphere(20);
 sphereVertices = [x(:) y(:) z(:)];
-
 %% Loop through and render on the plot
 clf;
 for values = transformationValues'
-    values = values';
-    eulerIntegration3dFromValues(values, sphereVertices, 10, 10);
+    eulerIntegration3dFromValues(values', sphereVertices, 10, 10);
 end
 axis image
 xlabel('X Axis');
