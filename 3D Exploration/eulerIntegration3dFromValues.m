478e58c4e36750633282d12df64801448caf84b0
Matthew Sundberg <matthberg@gmail.com>
2016-06-28 10:37:17 -0700

    fixed up the test functions, removed unneeded scripts

--- 3D Exploration/eulerIntegration3dFromValues.m
+++ 3D Exploration/eulerIntegration3dFromValues.m
@@ -9,8 +9,7 @@ stepMatrix(4,4) = 1;
 ptsOut = ones([size(pts,1) size(pts,2)+1 stepNums]); % Done strange so padding 1 for 4x4 transformations
 ptsOut(:,1:3,1) = pts;
 %% Coloring for testing data
-clf, close all;
-
+% clf, close all;
 cmap = parula(stepNums);
 plot3(ptsOut(:,1,1),ptsOut(:,2,1),ptsOut(:,3,1),'.-','Color',cmap(1,:));
 axis image
@@ -21,6 +20,6 @@ for index = 2:stepNums
     %% Test the transformation
     plot3(ptsOut(:,1,index),ptsOut(:,2,index),ptsOut(:,3,index),'.-','Color',cmap(index,:));
     hold on
-    pause
+    % pause
 end
 end
--- 3D Exploration/makeTransformationMatrix.m
+++
@@ -1,8 +0,0 @@
-function [ transformationMatrices ] = makeTransformationMatrix( transformationValues )
-%MAKETRANSFORMATIONMATRIX Generates translation matrices from a set of points to transform from the origin
-%% Initialize container and loop through input values
-transformationMatrices = zeros(4,4,size(transformationValues,1));
-for index = 1:size(transformationValues,1)
-    transformationMatrices(:,:,index) = makehgtform('translate',transformationValues(index,1:3),'axisrotate',transformationValues(index,4:6),transformationValues(index,7));
-end
-end
