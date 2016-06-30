d7e9bc325da0675a767021808beb07db211f7ddf
Matthew Sundberg <matthberg@gmail.com>
2016-06-29 17:08:00 -0700

    cleaned branch

--- 2D Simulation/matrixTranslate.m
+++
@@ -1,9 +0,0 @@
-%Returns a matrix for translating by (x, y) units
-function m = matrixTranslate(x, y)
-%Start w/ identity
-m = eye(3, 3);
-%Set x-distance
-m(1, 3) = x;
-%Set y-distance
-m(2, 3) = y;
-end
--- Data Reader/Read3dArray.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = Read3dArray( input_args )
-%READ3DARRAY Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
