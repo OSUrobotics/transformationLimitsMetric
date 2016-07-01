9d1547d09dfd7effbb613eb0954fd56bdfb779f0
Matthew Sundberg <matthberg@gmail.com>
2016-06-30 18:49:29 -0700

    rewrote some of the runscript

--- runSimulation.m
+++ runSimulation.m
@@ -2,24 +2,24 @@
 %   Loads an object PLY file, a pre-positioned hand STL, and some settings 
 %   and returns the area overlap of the two objects at evenly distributed 
 %   transformations.
-
+%% Declare variables and start parallel pool
+parpool(4);
 path2object = 'PitcherAssmMTest.ply';
 path2hand = 'roboHand.stl';
 objectScaleFactor = 5;
 handScaleFactor = 15;
 transformationScaleFactor = 20;
-numDirectionPoints = 20;
-numOrientationPoints = 15;
-angleDistribution = 10;
+numDirectionPoints = 10;
+numOrientationPoints = 10;
+angleDistribution = 5;
 interpolationNumber = 10;
 voxelResolution = 0.5;
 pmDepth = 4;
 pmScale = 1;
-outputFile = 'Out.mat';
-
-%==========================================================================
-
-bar = waitbar(0,'Starting Sumulation...','Name','Running Grasp Simulation...');
+outputFilePath = 'Output/S%iAreaIntersection.csv';
+tableHeaders = {'Timestamp','X Translation','Y Translation','Z Translation','Quaternion Value 1','Quaternion Value 2','Quaternion Value 3','Quaternion Value 4','Percent Volume Intersection'};
+%RUNSIMULATION loads an object PLY file, a pre-positioned hand STL, and some settings and returns the area overlap of the two objects at evenly distributed transformations 
+disp('Started Script');
 %% Load the object and scale to origin
 [objectV,objectF] = read_ply(path2object); % Gives vertical vertices matrix,association matrix
 objectVpad = [objectV ones(size(objectV,1),1)]; % Pad the points list with ones to work with 4x4 transformation matrices
@@ -33,36 +33,49 @@ handVpad = [handV ones(size(handV,1),1)];
 handVpad = handVpad*(makehgtform('translate',-getCentroidMesh(handV)).');
 handVpad = handVpad*(makehgtform('scale',handScaleFactor/max(abs(handV(:)))).');
 handV = handVpad(:,1:3);
+disp('Loaded and scaled objects');
+%% Display the hand and object
+stlPlot(objectV,objectF,true);
+stlPlot(handV,handF,true,'Object & Hand');
+camlight('headlight');
+material('dull');
 %% Generate transformation directions and orientations
-stlPlot(objectV,objectF,true,'Object');
-stlPlot(handV,handF,true,'Hand');
 transformationValues = makeTransformationValues(numDirectionPoints,numOrientationPoints,angleDistribution); % Use the function to generate the matrix of combinations
 %% Loop through and render on the plot
 %clf;
-outputArray = zeros(interpolationNumber,9,size(transformationValues, 2));
+outputMatrix = zeros(interpolationNumber,9,size(transformationValues, 2));
 tic;
-for valueIndex = 1:size(transformationValues, 2) % For every combination of values
-    %% Use euler integration generate all steps along the path
+lengthValues = size(transformationValues, 2);
+% bar = waitbar(0,'Starting Loop...','Name','Running Grasp Simulation...');
+parfor valueIndex = lengthValues % For every combination of values
+    %% Transform to all locations
     values = transformationValues(:,valueIndex); % Get the set of values
     [ptsOut,positionTransformsVector,~] = eulerIntegration3dFromValues(values,objectV,interpolationNumber,transformationScaleFactor); % Transform the object to each step
-    %visualizeTransformations(ptsOut, objectF);
     [voxOut,~,~] = eulerIntegration3dFromValues(values,objectVox,interpolationNumber,transformationScaleFactor); % Transform voxels as well
-    %% Get the collision data from every step
-    percentages = zeros(1,size(ptsOut,3));
-    for i = 1:size(ptsOut, 3)
+    %% Volume of intersection
+    percentages = zeros(1,interpolationNumber); % Preallocate
+    for i = 1:size(ptsOut, 3) % For every step, get the percent collision and add it to percentages
         percentages(i) = getPercentCollisionWithVerts(ptsOut(:,:,i),voxOut(:,:,i),handV,handF,voxelResolution,pmDepth,pmScale);
     end
-    %% Store the data in the output array
-    for i = 1:interpolationNumber
-        outputArray(i,:,valueIndex) = [i positionTransformsVector(:,i).' percentages(i)];
-    end
+    %% Write to matrix
+    outputMatrix(:,:,valueIndex) = [(1:interpolationNumber).' positionTransformsVector.' percentages(1:interpolationNumber).'];
     %% Update the loading bar
-    waitbar(valueIndex / size(transformationValues, 2),bar,sprintf('Simulating... (%i/%i)', valueIndex,size(transformationValues,2)));
+    fprintf('Done with value set %i/%i\n',valueIndex,lengthValues);
+    % waitbar(valueIndex / size(transformationValues, 2),bar,sprintf('Simulating... (%i/%i)', valueIndex,size(transformationValues,2)));
 end
+%% End the timer and progressbar
+disp('Done looping');
 toc;
 close(bar);
-%% Finalize data (Export or something)
-axis image
-xlabel('X Axis');
-ylabel('Y Axis');
-zlabel('Z Axis');
+%% Remap output to timestamp pages
+outputMatrix = permute(outputMatrix,[3 2 1]);
+%% Save to file
+for i = 1:size(outputMatrix,3)
+    outputTable = cell2table(outputMatrix(:,:,i), 'VariableNames', tableHeaders);
+    writetable(outputTable, sprintf(outputFilePath,i));
+    fprintf('File written for time %i\n',i);
+end
+%% End of script, kill parallel pool
+p = gcp;
+delete(p);
+disp('Done with script');
