6a5ba13da6d6047c124a4837a519d88e88f8f1f6
Matthew Sundberg <matthberg@gmail.com>
2016-07-01 10:25:22 -0700

    changes to saving

--- 3D Exploration/testFunctions.m
+++ 3D Exploration/testFunctions.m
@@ -1,6 +1,5 @@
 %% Load the files and prepare for graphing
-stlPath = 'stlTools/femur_binary.stl'; % sample object
-load('../transformationValues.mat');
+stlPath = 'Ball.stl'; % sample object
 figure
 [vertices,faces,~,~] = stlReadBinary(stlPath); % read stl in as faces and vertex
 %% General interesting information about file
@@ -8,8 +7,7 @@ fprintf('Number of vertices: %i\n',size(vertices,1));
 fprintf('Centroid of Mesh: (%0.5f, %0.5f, %0.5f)\n',getCentroidMesh(vertices));
 
 %% Make transformation values
-% transformationValues = makeTransformationValues(2,2,5);
-transformationValues = valuesIn;
+transformationValues = makeTransformationValues(20,20,10);
 transformationValues = transformationValues(1:30,:);
 %% Make example object
 [x,y,z] = sphere(20);
@@ -17,9 +15,9 @@ sphereVertices = [x(:) y(:) z(:)];
 stepNums = 10;
 %% Loop through and render on the plot
 clf;
-for values = transformationValues.'
-    [ ptsOut, positionTransformsVector, positionTransformsMatrix ] = eulerIntegration3dFromValues(values, sphereVertices, stepNums, 10);
-    visualizeTransformations(vertices,faces);
+for values = transformationValues
+    [ vertices, positionTransformsVector, positionTransformsMatrix ] = eulerIntegration3dFromValues(values, vertices, stepNums, 10);
+    visualizeTransformations(vertices);
 end
 axis image
 xlabel('X Axis');
--- Readers and Visualizations/saveGraphics.m
+++ Readers and Visualizations/saveGraphics.m
@@ -1,9 +1,8 @@
-function saveGraphics( filename, resolution )
-%SAVEGRAPHICS write to file a png with the resolution requested, in [height width] format
-resolution = resolution;
+function saveGraphics( filename, dimensions )
+%SAVEGRAPHICS write to file a png with the dimensions requested, in [height width] format
+dimensions = dimensions/10;
 set(gcf, 'PaperUnits','inches')
-set(gcf, 'PaperPosition', [0 0 1 resolution(1)/resolution(2)])
-
-print(gcf,filename,'-dpng',sprintf('-r%i',resolution(1)));
+set(gcf, 'PaperPosition', [0 0 10 10*dimensions(1)/dimensions(2)])
+print(gcf,filename,'-dpng',sprintf('-r%i',dimensions(2)));
 end
 
