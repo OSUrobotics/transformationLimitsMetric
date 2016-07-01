0f555b3cdb92fb19f8d453cb4a2f85a9ea565d56
RJSherman <rjsherman8888@gmail.com>
2016-07-01 09:38:44 -0700

    Cleaning out this branch

--- saveGraphics.m
+++
@@ -1,9 +0,0 @@
-function saveGraphics( filename, resolution )
-%SAVEGRAPHICS write to file a png with the resolution requested, in [height width] format
-resolution = resolution;
-set(gcf, 'PaperUnits','inches')
-set(gcf, 'PaperPosition', [0 0 1 resolution(1)/resolution(2)])
-
-print(gcf,filename,'-dpng',sprintf('-r%i',resolution(1)));
-end
-
