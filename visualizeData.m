5ed40db2b08dc059d0038c5d24ed3762a7ef79ab
Matthew Sundberg <matthberg@gmail.com>
2016-06-30 23:45:42 -0700

    improving visualize

--- visualizeData.m
+++ visualizeData.m
@@ -1,9 +1,9 @@
 % function [ output_args ] = visualizeData( input_args )
-clear, clf;
+clear, clf, close all;
 path2object = 'BallOut.ply';
 path2hand = 'CerealBox.stl';
 numSteps = 5;
-collisionThreshold = 0.0;
+collisionThreshold = 0.01;
 objectScaleFactor = 5;
 handScaleFactor = 15;
 dataFilePath = 'Output/S%iAreaIntersection.csv';
@@ -24,26 +24,28 @@ disp('Loaded and scaled objects');
 for step = numSteps
     fromTable = readtable(sprintf(dataFilePath,step));
     data = table2array(fromTable);
-    data(data(:,9)>collisionThreshold, :) = 0;
+    data(data(:,9)<collisionThreshold, :) = 0;
     transformations(:,:,step) = data(:,2:9);
 end
-
+disp('Filtered and realigned transformations');
+%% 
 ptsOut = zeros(size(objectVpad,1),4,length(numSteps));
 for transformIndex = 1:size(transformations,1)
     transformationStep = permute(transformations(transformIndex,:,:),[3 2 1]);
-        for step = 1:size(transformationStep,1)
-            %% Make the transformation matrix
-            transformationMatrix = eye(4);
-            transformationMatrix(1:3,1:3) = quat2dcm(transformationStep(step,4:7));
-            transformationMatrix = (makehgtform('translate',transformationStep(step,1:3))*transformationMatrix).';
-            ptsOut(:,:,step) = objectVpad*transformationMatrix;
-        end
-        visualizeTransformations(ptsOut(:,1:3,:));
-        hold on
+    for step = 1:size(transformationStep,1)
+        %% Make the transformation matrix
+        transformationMatrix = eye(4);
+        transformationMatrix(1:3,1:3) = quat2dcm(transformationStep(step,4:7));
+        transformationMatrix = (makehgtform('translate',transformationStep(step,1:3))*transformationMatrix).';
+        ptsOut(:,:,step) = objectVpad*transformationMatrix;
+    end
+    visualizeTransformations(ptsOut(:,1:3,:));
+    hold on
 end
+disp('Plotted');
 stlPlot(handV,handF);
 axis image;
 camlight('headlight');
 material('dull');
+disp('Done');
 % end
-
