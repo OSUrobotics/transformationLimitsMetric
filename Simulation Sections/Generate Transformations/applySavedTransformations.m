8be13068323becdad1fee91393ba22cdfb02823d
Matthew Sundberg <matthberg@gmail.com>
2016-07-05 15:49:28 -0700

    fixing/changing up the trajectory selection and application

--- Simulation Sections/Generate Transformations/applySavedTransformations.m
+++ Simulation Sections/Generate Transformations/applySavedTransformations.m
@@ -1,25 +1,27 @@
-function [ ptsTransformed ] = applySavedTransformations( transformsFilename, pts, vertical )
+function [ ptsTransformed ] = applySavedTransformations( transformsFilename, pts, verticalOut )
 %% APPLYSAVEDTRANSFORMATIONS takes saved transformation steps and applies them to create a multi-dimensional indexed set of points
 %   Detailed explanation goes here
-%% Read in and reshape to proper size
-read([transformsFilename '.mat']);
-transformSteps = transformsFilename;
+%% Read in structure
+load([transformsFilename '.mat']);
+% transformationStruct.trajectorySteps is a 4d set of transformations in [4x4 transformation matrix, step, values set index] 
 %% Reformat pts if not in the right dimensions to be horizontal and pad
 if size(pts,2) == 3 && size(pts,1) ~= 3 % if in a vertical format
     pts = pts.';
 end
 pts = [pts; ones(1,size(pts,2))];
 %% Initialize matrix for output
-ptsTransformed = zeros(4,size(pts,2),size(transformSteps,3));
-%% Loop and apply the transformation
-for stepIndex = 1:size(transformSteps,3)
-    ptsTransformed(:,:,stepIndex) = transformSteps(:,:,stepIndex) * pts;
+ptsTransformed = zeros(4,size(pts,2),size(transformationStruct.trajectorySteps,3),size(transformationStruct.trajectorySteps,4));
+%% Loop through value sets
+for valueIndex = 1:size(transformationStruct.trajectorySteps,4)
+    for stepIndex = 1:size(transformationStruct.trajectorySteps,3)
+        ptsTransformed(:,:,stepIndex,valueIndex) = transformationStruct.trajectorySteps(:,:,stepIndex,valueIndex)*pts;
+    end
 end
 %% Remove padding and reformat if wanted
-ptsTransformed = ptsTransformed(1:3,:,:);
+ptsTransformed = ptsTransformed(1:3,:,:,:);
 if nargin == 3
-    if vertical
-        ptsTransformed = permute(ptsTransformed,[2 1 3]);
+    if verticalOut
+        ptsTransformed = permute(ptsTransformed,[2 1 3 4]);
     end
 end
 end
--- Simulation Sections/Generate Transformations/saveTrajectories.m
+++ Simulation Sections/Generate Transformations/saveTrajectories.m
@@ -1,12 +1,21 @@
-function saveTrajectories( numTranslationDirections, numRotationAxes, numAngleDivisions, numInterpolationSteps, translateScalar, filename)
+function [ transformationStruct ] = saveTrajectories( numTranslationDirections, numRotationAxes, numAngleDivisions, numInterpolationSteps, translateScalar, filename, divisionRange)
 %% SAVETRAJECTORIES Uses the other functions to generate and save a 4D matrix with all of the transformations interpolated
-%   
+%% Make a parpool 
+p = gcp('nocreate');
+if isempty(p)
+    parpool(8);
+end
 %% Generate the trajectories interpolated along
 values = generateTrajectories(numTranslationDirections, numRotationAxes, numAngleDivisions);
+%% If want subselection, apply it here
+if nargin == 7
+    values = values(:,divisionRange);
+end
 %% Prepare for and loop through creating the steps
-trajectorySteps = zeros(4,4,numInterpolationSteps,size(values,1));
-for transformIndex = 1:size(values,1)
-    trajectorySteps(:,:,:,transformIndex) = trajectoryStepsEXPM(values(transformIndex,:),numInterpolationSteps,translateScalar);
+trajectorySteps = zeros(4,4,numInterpolationSteps,size(values,2));
+parfor transformIndex = 1:size(values,2)
+    trajectorySteps(:,:,:,transformIndex) = trajectoryStepsEXPM(values(:,transformIndex),numInterpolationSteps,translateScalar);
+    fprintf('Done with value set #%i/%i\n',transformIndex,size(values,2));
 end
 %% Save to structure
 transformationStruct = struct;
@@ -20,4 +29,3 @@ transformationStruct.values = values;
 %% Save to file
 save([filename '.mat'],'transformationStruct','-mat');
 end
-
