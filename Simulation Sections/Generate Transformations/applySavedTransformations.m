f722b236e19946cabc2cd1f7657de4a0df75d199
Matthew Sundberg <matthberg@gmail.com>
2016-07-05 10:39:06 -0700

    put in a script to read and apply transformations

---
+++ Simulation Sections/Generate Transformations/applySavedTransformations.m
@@ -0,0 +1,25 @@
+function [ ptsTransformed ] = applySavedTransformations( transformsFilename, pts, vertical )
+%% APPLYSAVEDTRANSFORMATIONS takes saved transformation steps and applies them to create a multi-dimensional indexed set of points
+%   Detailed explanation goes here
+%% Read in and reshape to proper size
+transformSteps = reshape(csvread(transformsFilename),4,4,[]);
+%% Reformat pts if not in the right dimensions to be horizontal and pad
+if size(pts,2) == 3 && size(pts,1) ~= 3 % if in a vertical format
+    pts = pts.';
+end
+pts = [pts; ones(1,size(pts,2))];
+%% Initialize matrix for output
+ptsTransformed = zeros(4,size(pts,2),size(transformSteps,3));
+%% Loop and apply the transformation
+for stepIndex = 1:size(transformSteps,3)
+    ptsTransformed(:,:,stepIndex) = transformSteps(:,:,stepIndex) * pts;
+end
+%% Remove padding and reformat if wanted
+ptsTransformed = ptsTransformed(1:3,:,:);
+if nargin == 3
+    if vertical
+        ptsTransformed = permute(ptsTransformed,[2 1 3]);
+    end
+end
+end
+
