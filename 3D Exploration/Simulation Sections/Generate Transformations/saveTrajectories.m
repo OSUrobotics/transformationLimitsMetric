f79c77b7112a67a4830b12e816111ab2b41b2138
Matthew Sundberg <matthberg@gmail.com>
2016-07-07 13:49:55 -0700

    adding documentation to new trajectories

--- 3D Exploration/Simulation Sections/Generate Transformations/saveTrajectories.m
+++ 3D Exploration/Simulation Sections/Generate Transformations/saveTrajectories.m
@@ -1,16 +1,34 @@
-function [ transformationStruct ] = saveTrajectories( numTranslationDirections, numRotationAxes, numAngleDivisions, numInterpolationSteps, translateScalar, filename, divisionRange)
+function [ transformationStruct ] = saveTrajectories( numTranslationDirections, numRotationAxes, angleDivisions, numInterpolationSteps, translateScalar, filename, divisionRange)
 %% SAVETRAJECTORIES Uses the other functions to generate and save a 4D matrix with all of the transformations interpolated
+% INPUTS:
+%     numTranslationDirections - How many directions to translate the object in, excluding rotation, when defining the values for the possible transformations
 % 
+%     numRotationAxes - Mandatory int - How many axes to rotate the object around when defining the values for the possible transformations
+% 
+%     angleDivisions - Mandatory vector - The angle amounts for rotation around each axis, given in a -1 to 1 range which is then mapped to -pi to pi. 
+% 
+%     numInterpolationSteps - Mandatory int - The number of steps in the "animation", how many interpolated stages of the transformation are returned. 
+% 
+%     translateScalar - Optional double - The length of the interpolation path. Is roughly similar to the distance away from the origin which the object is transformed to. By default = 1.
+% 
+%     filename - Optional string - Saves to a mat file with this name, if included. Enter the string relative to the current directory and without the .mat extension, that is added later. When not included just outputs the structure. 
+% 
+%     divisionRange - Optional vector - Saves the trajectories for only those value sets, if you select 1:100 or [1 2 5:60] it will only return the transformations for that subselection of total values. When not included saves all value sets. 
+
 %% Make a parpool 
 p = gcp('nocreate');
 if isempty(p)
     parpool(7);
 end
 %% Generate the trajectories interpolated along
-values = generateTrajectories(numTranslationDirections, numRotationAxes, numAngleDivisions);
+values = generateTrajectories(numTranslationDirections, numRotationAxes, angleDivisions);
 %% If want subselection, apply it here
 if nargin == 7
     values = values(:,divisionRange);
+end
+%% Set default translateScalar
+if nargin == 5
+    translateScalar = 1;
 end
 %% Prepare for and loop through creating the steps
 trajectorySteps = zeros(4,4,numInterpolationSteps,size(values,2));
@@ -25,12 +43,14 @@ end
 transformationStruct = struct;
 transformationStruct.numTranslationDirections = numTranslationDirections;
 transformationStruct.numRotationAxes = numRotationAxes;
-transformationStruct.numAngleDivisions = numAngleDivisions;
+transformationStruct.numAngleDivisions = angleDivisions;
 transformationStruct.numInterpolationSteps = numInterpolationSteps;
 transformationStruct.translateScalar = translateScalar;
 transformationStruct.trajectorySteps = trajectorySteps;
 transformationStruct.values = values;
 transformationStruct.stepValues = stepValues;
-%% Save to file
-save([filename '.mat'],'transformationStruct','-mat');
+%% Save to file if included
+if nargin >= 6
+	save([filename '.mat'],'transformationStruct','-mat');
+end
 end
