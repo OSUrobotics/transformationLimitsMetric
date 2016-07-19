5a1274ed7de96784765fe635c04774b9af5a6f0e
RJSherman <rjsherman8888@gmail.com>
2016-07-19 12:34:03 -0700

    Added docs to getCollisionVoxelVoxel

--- 3D Exploration/Simulation Sections/Collision Detection/getCollisionVoxelVoxel.m
+++ 3D Exploration/Simulation Sections/Collision Detection/getCollisionVoxelVoxel.m
@@ -1,6 +1,43 @@
 function [ amountCollide ] = getCollisionVoxelVoxel( handVox, objectVox, objectSurfPoints, objectSurfaceArea, resolution, method )
-%% GETCOLLISIONVOXELVOXEL Gets the amount of collisions between a set of voxels in both count and summed internal distance, given voxel coordinates Nx3 matrix, and a Nx4 matrix defining the SDF values at the meshgrid coordinates
-%   Detailed explanation goes here
+%% GETCOLLISIONVOXELVOXEL Gets the amount of collisions between two voxel sets
+% a set of voxels in both count and summed internal distance, given voxel coordinates Nx3 matrix, and a Nx4 matrix defining the SDF values at the meshgrid coordinates
+%==========================================================================
+%
+% USAGE
+%       [ amountCollide ] = getCollisionVoxelVoxel( handVox, objectVox, objectSurfPoints, objectSurfaceArea, resolution, method )
+%
+% INPUTS
+%
+%       handVox             - Mandatory - Nx4 array         - List of voxel data for the hand where the rows hold x,y,z values and a 1 or 0 for inside or outside, and where N is the number of points
+%
+%       objectVox           - Mandatory - Nx3 array         - List of the object's voxel coordinates where N is the number of voxels
+%
+%       objectSurfPoints    - Mandatory - Nx3 array         - List of the object's surface samples (Poisson disk sampling of points out of meshLab) where N is the number of points
+%
+%       objectSurfaceArea   - Mandatory - Double Value      - Total surface area of the object
+%
+%       resolution          - Mandatory - Double Value      - The resolution passed to getVoxelisedVerts when generating objectVox
+%
+%       method              - Mandatory - Interp3 Meathod   - The type of interpolation to use
+%
+% OUTPUTS
+%
+%       amountCollide       - Mandatory - Double Value      - Total collision value that represents the amount of the object inside the hand
+%
+% EXAMPLE
+%
+%       To get the collision of a pitcher with a BH8-280:
+%       >>  [ amountCollide ] = getCollisionVoxelVoxel( BH8-280voxelValues, pitcherVoxelisedVerts, pitcherPoissonSamples, pitcherSurfaceArea, pitcherVoxelisedVertsResolution, 'cubic' )
+%
+% NOTES
+%
+%   -Both meshes must be properly closed (ie. watertight).
+%   -objectSurfPoints must match the mesh used to generate objectVox.
+%   -The resolution value must match the one passed to getVoxelisedVerts 
+%    to get the voxel data for the object.
+%
+%==========================================================================
+
 valuesAtVoxels = interp3(handVox(:,:,:,1),handVox(:,:,:,2),handVox(:,:,:,3),handVox(:,:,:,4),objectVox(:,1),objectVox(:,2),objectVox(:,3),method);
 %% Filter by only inside
 valuesAtVoxels = valuesAtVoxels(valuesAtVoxels>0.5);
