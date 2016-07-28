8cdd6f5e927bc1796d17f989177ef7f6cc862308
RJSherman <rjsherman8888@gmail.com>
2016-07-28 09:10:04 -0700

    Messy changes that need a more elegant implementation in the future, but work for now

--- 3D Exploration/filenamesFromComponents.m
+++ 3D Exploration/filenamesFromComponents.m
@@ -1,17 +1,17 @@
 function [ transformationFP, objectFP, surfaceFP, handFP, outputFP ] = filenamesFromComponents( objectNum, subjectNum, graspNum, extreme, stepNum )
 %% FILENAMESFROMCOMPONENTS Takes in filepath components and outputs the filenames for the hand, object, transformations, and output file
 % stepNum - Optional - use only if want a specific output, can use single number or range, range leads to cell array output
-transformationFP = sprintf('handAndAlignment/transforms/obj%i_sub%i_grasp%i_%s_object_transform.txt',objectNum,subjectNum,graspNum,extreme);
+transformationFP = sprintf('handAndAlignment/transforms/obj%i_sub%i_grasp%i_%s_Transformation.txt',objectNum,subjectNum,graspNum,extreme);
 handFP = sprintf('handAndAlignment/hand/obj%i_sub%i_grasp%i_%s.stl',objectNum,subjectNum,graspNum,extreme);
 if nargin == 5 && length(stepNum) ~= 1
     outputFP = cell(1,length(stepNum));
     for stepNumIndex = 1:length(stepNum)
-        outputFP{stepNumIndex} = sprintf('Output/Step%iObject%iSubject%iGrasp%i%sAreaIntersection.csv',stepNum(stepNumIndex),objectNum,subjectNum,graspNum,extreme);
+        outputFP{stepNumIndex} = sprintf('Output/Step%iobj%i_sub%i_grasp%i_%sAreaIntersection.csv',stepNum(stepNumIndex),objectNum,subjectNum,graspNum,extreme);
     end
 elseif nargin == 5
-    outputFP = sprintf('Output/Step%iObject%iSubject%iGrasp%i%sAreaIntersection.csv',stepNum,objectNum,subjectNum,graspNum,extreme);
+    outputFP = sprintf('Output/Step%iobj%i_sub%i_grasp%i_%sAreaIntersection.csv',stepNum,objectNum,subjectNum,graspNum,extreme);
 else
-    outputFP = sprintf('Output/Step%sObject%iSubject%iGrasp%i%sAreaIntersection.csv','%i',objectNum,subjectNum,graspNum,extreme);
+    outputFP = sprintf('Output/Step%sobj%i_sub%i_grasp%i_%sAreaIntersection.csv','%i',objectNum,subjectNum,graspNum,extreme);
 end
 objectLinking = table2cell(readtable('handAndAlignment/obj_dict.csv'));
 objectFP = sprintf('sampleSTLs/%s',objectLinking{[objectLinking{:,1}] == objectNum,3});
--- 3D Exploration/linkFilenames.m
+++ 3D Exploration/linkFilenames.m
@@ -3,8 +3,8 @@ function [ valuesOut ] = linkFilenames( outputFilePath )
 %% Declaring variables
 path2transforms = 'handAndAlignment/transforms/';
 format4hands = 'handAndAlignment/hand/obj%i_sub%i_grasp%i_%s.stl';
-format4output = 'Output/Step%sObject%iSubject%iGrasp%i%sAreaIntersection.csv';
-format4transforms = 'obj%d_sub%d_grasp%d_%s_object_transform.txt';
+format4output = 'Output/Step%sobj%isub%igrasp%i%sAreaIntersection.csv';
+format4transforms = 'obj%d_sub%d_grasp%d_%s_Transformation.txt';
 path2numObjectReference = 'handAndAlignment/obj_dict.csv';
 path2objects = 'sampleSTLs/';
 tableHeaders = {'TransformsFilepath','ObjectFilepath','ObjectSurfaceFilepath','HandFilepath','ObjectNum','SubjectNum','GraspNum','Extreme','OutputFormat'};
@@ -17,13 +17,13 @@ dirOut = dir(path2transforms);
 names = {dirOut.name};
 names = names(3:end); % Prune . and ..
 %% Scan in the format for the transformation names
-valuesOut = cell(length(names),9);
+valuesOut = cell(length(names),8);
 for nameIndex = 1:length(names)
     %% Read in the filename of the transformation
     valuesOut(nameIndex,5:8) = textscan(names{nameIndex},format4transforms);
     %% Get only string out
     valuesOut{nameIndex,8} = valuesOut{nameIndex,8}{1};
-    valuesOut{nameIndex,8} = valuesOut{nameIndex,8}(1:8);
+    valuesOut{nameIndex,8} = valuesOut{nameIndex,8}(1:15);
     %% Extract other filepaths
     [valuesOut{nameIndex,1}, valuesOut{nameIndex,2}, valuesOut{nameIndex,3}, valuesOut{nameIndex,4}, valuesOut{nameIndex,9}] = ...
         filenamesFromComponents(valuesOut{nameIndex,5},valuesOut{nameIndex,6},valuesOut{nameIndex,7}, ...
