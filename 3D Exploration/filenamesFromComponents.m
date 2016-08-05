c145c57bb97166f0cc00d02d5ad3449f7a1c1930
RJSherman <rjsherman8888@gmail.com>
2016-08-05 16:05:18 -0700

    Updated docs in the main folder

--- 3D Exploration/filenamesFromComponents.m
+++ 3D Exploration/filenamesFromComponents.m
@@ -1,6 +1,42 @@
 function [ transformationFP, objectFP, surfaceFP, handFP, outputFP ] = filenamesFromComponents( objectNum, subjectNum, graspNum, extreme, stepNum )
 %% FILENAMESFROMCOMPONENTS Takes in filepath components and outputs the filenames for the hand, object, transformations, and output file
-% stepNum - Optional - use only if want a specific output, can use single number or range, range leads to cell array output
+%==========================================================================
+%
+% USAGE
+%
+%       [ transformationFP, objectFP, surfaceFP, handFP, outputFP ] = filenamesFromComponents( objectNum, subjectNum, graspNum, extreme, stepNum )
+%
+% INPUTS
+%
+%       objectNum           - Mandatory - Integer Value             - Object Number component of the desired file name
+%
+%       subjectNum          - Mandatory - Integer Value             - Subject Number component of the desired file name
+%
+%       graspNum            - Mandatory - Integer Value             - Grasp Number component of the desired file name
+%
+%       extreme             - Mandatory - String                    - Following string of the desired file name
+%
+%       stepNum             - Optional  - Integer Value or Range    - Step Number component of the desired file name
+%
+% OUTPUTS
+%
+%       transformationFP    - Mandatory - Filepath String           - Path to the desired transformation matrix file
+%
+%       objectFP            - Mandatory - Filepath String           - Path to the object model file
+%
+%       surfaceFP           - Mandatory - Filepath String           - Path to the object surface samples file
+%
+%       handFP              - Mandatory - Filepath String           - Path to the hand model file
+%
+%       outputFP            - Mandatory - Filepath String           - Path to the simulation output file
+%
+% EXAMPLE
+%
+%       To get the paths to object 7, subject 9, grasp 2, 'extreme0_prime_' steps 1 to 9 :
+%       >>  [ transformationFP, objectFP, surfaceFP, handFP, outputFP ] = filenamesFromComponents( 7, 9, 2, 'extreme0_prime_', 1:9 )
+%
+%==========================================================================
+
 transformationFP = sprintf('handAndAlignment/transforms/obj%i_sub%i_grasp%i_%s_Transformation.txt',objectNum,subjectNum,graspNum,extreme);
 handFP = sprintf('handAndAlignment/hand/obj%i_sub%i_grasp%i_%s.stl',objectNum,subjectNum,graspNum,extreme);
 if nargin == 5 && length(stepNum) ~= 1
--- 3D Exploration/linkFilenames.m
+++ 3D Exploration/linkFilenames.m
@@ -1,5 +1,28 @@
 function [ valuesOut ] = linkFilenames( outputFilePath )
 %% LINKFILENAMES scans the handAndAlignment directories and files to create a csv file of the relations between those files, with one line per object-person-grasp-extremism set, with a filepath for the object STL, hand STL, and transformation matrices and output data filepaths
+%==========================================================================
+%
+% USAGE
+%
+%       [ valuesOut ] = linkFilenames( outputFilePath )
+%
+% INPUTS
+%
+%       outputFilePath  - Mandatory - Filepath String   - Name of the output csv
+%
+% OUTPUTS
+%
+%       valuesOut       - Mandatory - Cell Array        - Output table that is saved to outputFilePath:  Columns are: TransformsFilepath, ObjectFilepath, ObjectSurfaceFilepath, HandFilepath, ObjectNum, SubjectNum, GraspNum, Extreme, OutputFormat
+%
+% NOTES
+%
+%   -The stls for both the hand and the object must have come out of OpenRave.
+%   -The object must then be put through meshLab, and exported as a ply file containing the poisson-disk sampled points of the object. 
+%   -handSpreadDistance can be measured or gotten from the hand's datasheet.
+%
+%==========================================================================
+
+
 %% Declaring variables
 path2transforms = 'handAndAlignment/transforms/';
 format4transforms = 'obj%d_sub%d_grasp%d_%s_Transformation.txt';
